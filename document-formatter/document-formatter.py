caa93dcb500e107111bb7b174912233570baf189
CosmicCatnap <cosmiccatnap@gmail.com>
2022-04-27 22:49:18 -0400

    mostly works

--- document-formatter/document-formatter.py
+++ document-formatter/document-formatter.py
@@ -2,16 +2,34 @@
 
 import sys
 
-try:
-	with open(sys.argv[1], 'r') as doc:
-		document = doc.read()
-		formatted = document.split('. ')
-		for sentence in formatted:
-			if len(sentence) > 40:
-				sentence = sentence.rpartition(' ')
-				for part in sentence:
-					print(part)
+def tokenize(text):
+	sentenceList = text.split('.')
+	returnList = []
+	for sentence in sentenceList:
+		returnList.append(sentence.split())	
+	return returnList
+
+def readFile(filename):
+	try:
+		with open(filename, 'r') as file:
+			return file.read()
+	except IndexError:
+		print("ERROR: empty argument list, no text file was provided")
+
+def formattedPrint(sentenceList):
+	sentenceSize = 0
+	for sentence in sentenceList:
+		for word in sentence:
+			if sentenceSize > 40:
+				print(word, end='\n')
+				sentenceSize = 0
+			elif word.find('.') == -1:
+				print(word, end=' ')
 			else:
-				print(sentence)
-except IndexError:
-	print("ERROR: empty argument list, no text file was provided")
+				print(word, end='\n')	
+			sentenceSize += len(word)
+
+document = readFile(sys.argv[1])
+parsedDoc = tokenize(document)
+formattedPrint(parsedDoc)
+#print(parsedDoc)
